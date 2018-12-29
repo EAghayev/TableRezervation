7bf8ca1fd2f64cfd36ba978b5b509b70a5b595e3
EAghayev <41126467+EAghayev@users.noreply.github.com>
2018-12-29 10:27:03 +0400

    Add files via upload

---
+++ Rezervation/Rezervation/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Rezervation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.BtnRezerv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtRezervTime = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnCheck);
+            this.groupBox1.Controls.Add(this.BtnRezerv);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TxtRezervTime);
+            this.groupBox1.Location = new System.Drawing.Point(510, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 167);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Qeydiyyat";
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(6, 115);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(172, 23);
+            this.BtnCheck.TabIndex = 3;
+            this.BtnCheck.Text = "Hesabi cixart";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // BtnRezerv
+            // 
+            this.BtnRezerv.Location = new System.Drawing.Point(6, 79);
+            this.BtnRezerv.Name = "BtnRezerv";
+            this.BtnRezerv.Size = new System.Drawing.Size(172, 23);
+            this.BtnRezerv.TabIndex = 2;
+            this.BtnRezerv.Text = "Rezerv et";
+            this.BtnRezerv.UseVisualStyleBackColor = true;
+            this.BtnRezerv.Click += new System.EventHandler(this.BtnRezerv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Qeydiyyat muddeti";
+            // 
+            // TxtRezervTime
+            // 
+            this.TxtRezervTime.Location = new System.Drawing.Point(6, 42);
+            this.TxtRezervTime.Name = "TxtRezervTime";
+            this.TxtRezervTime.Size = new System.Drawing.Size(172, 20);
+            this.TxtRezervTime.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.Button BtnRezerv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtRezervTime;
+    }
+}
+
---
+++ Rezervation/Rezervation/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rezervation.Models;
+
+namespace Rezervation
+{
+    public partial class Form1 : Form
+    {
+        private readonly CabinetRezervationEntities db = new CabinetRezervationEntities();
+        private Cabinet SelectedCabinet;
+        private Button SelectedCabinetButton;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateCabinets();
+        }
+        public void CreateCabinets()
+        {
+            int X = 15;
+            foreach (var item in db.Cabinets)
+            {
+                Button btn = new Button();
+                btn.Name = "Btn" + item.Name;
+                btn.Text = item.Name;
+                if (item.IsReserved)
+                {
+                    btn.BackColor = Color.DarkRed;
+                }
+                else
+                {
+                    btn.BackColor = Color.GreenYellow;
+                }
+                btn.Width = 100;
+                btn.Height = 100;
+                btn.Location= new System.Drawing.Point(X, 42);
+                X += btn.Width + 20;
+                btn.Click+= new System.EventHandler(this.BtnCabinets_Click);
+                this.Controls.Add(btn);
+            }
+        }
+
+        private void BtnCabinets_Click(object sender, EventArgs e)
+        {
+            SelectedCabinetButton = sender as Button;
+            if (!String.IsNullOrWhiteSpace(SelectedCabinetButton.Text))
+            {
+                Cabinet cabinet = db.Cabinets.FirstOrDefault(c => c.Name == SelectedCabinetButton.Text);
+                if (cabinet != null)
+                {
+                    SelectedCabinet = cabinet;
+                    if (SelectedCabinet.IsReserved==false)
+                    {
+                        BtnRezerv.Enabled = true;
+                        BtnCheck.Enabled = false;
+                        SelectedCabinetButton.BackColor = Color.Pink;
+                        SelectedCabinetButton.FlatAppearance.BorderColor = Color.Plum;
+                    }
+                    else
+                    {
+                        BtnRezerv.Enabled = false;
+                        BtnCheck.Enabled = true;
+                    }
+                }
+            }
+
+        }
+
+        private void BtnRezerv_Click(object sender, EventArgs e)
+        {
+            if (SelectedCabinet.IsReserved==false)
+            {
+                DateTime StartDate = DateTime.Now;
+
+                CabinetRezervation rezervation = new CabinetRezervation
+                {
+                    CabinetId = SelectedCabinet.Id,
+                    StartDate = StartDate
+                };
+                if (!String.IsNullOrWhiteSpace(TxtRezervTime.Text))
+                {
+                    int Time = Convert.ToInt32(TxtRezervTime.Text);
+                    DateTime EndDate = StartDate.AddMinutes(Time);
+                    rezervation.EndDate = StartDate.AddMinutes(Time);
+                    rezervation.TotalPrice = SelectedCabinet.Price * (Convert.ToDecimal(EndDate.Subtract(StartDate).TotalMinutes / 60));
+                }
+                db.CabinetRezervations.Add(rezervation);
+                SelectedCabinet.IsReserved = true;
+                db.SaveChanges();
+                SelectedCabinetButton.BackColor = Color.DarkRed;
+            }
+            else
+            {
+                MessageBox.Show("Bu kabinet rezerv olunub");
+            }
+
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            if (SelectedCabinet.IsReserved)
+            {
+                CabinetRezervation rezervation = db.CabinetRezervations.OrderByDescending(c => c.StartDate).Where(c => c.CabinetId == SelectedCabinet.Id).First();
+                rezervation.TotalPrice = (Convert.ToDecimal(DateTime.Now.Subtract(rezervation.StartDate.Value).TotalSeconds) / 60) * SelectedCabinet.Price;
+                rezervation.EndDate = DateTime.Now;
+                SelectedCabinet.IsReserved = false;
+                db.SaveChanges();
+                MessageBox.Show(rezervation.TotalPrice.Value.ToString("0.00"));
+                SelectedCabinetButton.BackColor = SystemColors.Control;
+                BtnCheck.Enabled = false;
+                //BtnRezerv.Enabled = true;
+            }
+
+        }
+    }
+}
